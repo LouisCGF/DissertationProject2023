263039fc53ff7022c436e9bf5c4df006aebece43
LouisCGF <louis.ware@hotmail.co.uk>
2023-03-01 21:23:55 +0000

    Initial Commit

---
+++ DissertationProject2023/DissertationProject2023App.swift
@@ -0,0 +1,17 @@
+//
+//  DissertationProject2023App.swift
+//  DissertationProject2023
+//
+//  Created by Louis on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DissertationProject2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
